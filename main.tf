8d021c6b51b12a601877dc8a7402768852d89862
pakaashok <ashokp.linux@gmail.com>
2026-03-10 17:47:09 +0530

    test-module

---
+++ main.tf
@@ -0,0 +1,60 @@
+module "vpc" {
+
+  source = "git::https://github.com/pakaashok/terraform-modules.git//vpc"
+
+  vpc_name       = "demo-vpc"
+  vpc_cidr       = "10.0.0.0/16"
+  public_subnet  = "10.0.1.0/24"
+  private_subnet = "10.0.2.0/24"
+  az             = "eu-west-1a"
+}
+
+resource "aws_security_group" "ec2_sg" {
+
+  name   = "ec2-sg"
+  vpc_id = module.vpc.vpc_id
+
+  ingress {
+
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  egress {
+
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+data "aws_ami" "amazon_linux" {
+
+  most_recent = true
+
+  owners = ["amazon"]
+
+  filter {
+    name   = "name"
+    values = ["amzn2-ami-hvm-*-x86_64-gp2"]
+  }
+}
+
+resource "aws_instance" "public_ec2" {
+
+  ami           = data.aws_ami.amazon_linux.id
+  instance_type = "t2.micro"
+
+  subnet_id = module.vpc.public_subnet_id
+
+  vpc_security_group_ids = [aws_security_group.ec2_sg.id]
+
+  associate_public_ip_address = true
+
+  tags = {
+    Name = "demo-ec2"
+  }
+}
